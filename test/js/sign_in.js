4a41ebc1b83907097eed1cad6f4e18a3467a073d
username github <email github>
2021-08-27 01:29:38 +0700

    27-08

--- test/js/sign_in.js
+++ test/js/sign_in.js
@@ -104,3 +104,31 @@ function logIn(LoginData){
         alert("Login thành công!")
     }
 }
+
+const signOutLink = document.getElementById("sign-out")
+signOutLink.addEventListener("click", () => {
+    if (localStorage.getItem("currentUser") == null) {
+        alert("Bạn chưa đăng nhập, hiện không thể đăng xuất!")
+    }
+    else{
+        localStorage.removeItem("currentUser")
+        location.reload()
+    }
+})
+
+if (localStorage.getItem("currentUser") == null) {
+    signOutLink.innerHTML = ""
+}
+else{
+    signOutLink.innerHTML = "SIGN OUT"
+}
+
+
+
+let signInLink = document.getElementById("in")
+if (localStorage.getItem("currentUser") == null) {
+    signInLink.innerHTML = "SIGN IN"
+}
+else{
+    signInLink.innerHTML = ""
+}
--- test/js/sign_up.js
+++ test/js/sign_up.js
@@ -106,3 +106,31 @@ function signUp(signUpData){
           alert("Đăng kí thành công, vui lòng trở về đăng nhập!")
         }
 }
+
+const signOutLink = document.getElementById("sign-out")
+signOutLink.addEventListener("click", () => {
+    if (localStorage.getItem("currentUser") == null) {
+        alert("Bạn chưa đăng nhập, hiện không thể đăng xuất!")
+    }
+    else{
+        localStorage.removeItem("currentUser")
+        location.reload()
+    }
+})
+
+if (localStorage.getItem("currentUser") == null) {
+    signOutLink.innerHTML = ""
+}
+else{
+    signOutLink.innerHTML = "SIGN OUT"
+}
+
+
+
+let signInLink = document.getElementById("in")
+if (localStorage.getItem("currentUser") == null) {
+    signInLink.innerHTML = "SIGN IN"
+}
+else{
+    signInLink.innerHTML = ""
+}
